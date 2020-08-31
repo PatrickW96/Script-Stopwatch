6335f93f1f4f9a781e596cd3d5941b8d04dbd388
Admin <admin@Admins-MacBook-Pro.local>
2020-08-30 20:01:37 -0400

    adding javascript files

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
+/* Defining Time Values for Stop Watch */
+var seconds = 0;
+var minutes = 0;
+var hours = 0;
 
+var displaySeconds = 0;
+var displayMinutes = 0;
+var displayHours = 0;
+
+/* Setting up the function that sets the values relative to time and increments those set values accordingly */
+function stopWatch() {
+    seconds++; 
+
+    if(seconds / 60 === 1) {
+        seconds = 0;
+        minutes++;
+    } 
+    if (minutes / 60 === 1) {
+        minutes = 0;
+        hours++; 
+    }
+
+    if (seconds < 10) {
+        displaySeconds = "0" + seconds;
+    } else {
+        displaySeconds = seconds;
+    }
+
+
+
+    document.getElementById("display").innerHTML = hours + ":" + minutes + ":" + seconds;
+
+}
+
+//window.setInterval(stopWatch, 1000)
