c6673d473670ae768ec70076d8885113c51fce82
Patrick W <pat.realrealestatellc@gmail.com>
2020-09-03 02:33:28 -0400

    Stopwatch is working beautifully. Need to add some business ads and more CSS and this will be a finished project

--- js/script.js
+++ js/script.js
@@ -4,9 +4,15 @@ var minutes = 0;
 var hours = 0;
 
 /* Creating var to hold the values that are going to be displayed to user */
-dispalySeconds = 0;
-displayMinutes = 0;
-displayHours = 0;
+dispalyedSec = 0;
+displayedMin = 0;
+displayedHours = 0;
+
+/* Creating a var to hold value of watch on/off status */
+var watchOnOff = 0;
+
+/* Defining a var to hold the val of setInterval function */
+var timer;
 
 /* Creating the stopwatch function */
 function stopWatch() {
@@ -22,30 +28,57 @@ function stopWatch() {
 
 /* Defining displayValues */
     if (seconds < 10) {
-        displaySeconds = "0" + seconds;
+        displayedSec = "0" + seconds;
     } else {
-        displaySeconds = seconds;
+        displayedSec = seconds;
     }
 
     if (minutes < 10) {
-        displayMinutes = "0" + minutes;
+        displayedMin = "0" + minutes;
     } else {
-        displayMinutes = minutes;
+        displayedMin = minutes;
     }
 
     if (hours <10) {
-        displayHours = "0" + hours;
+        displayedHours = "0" + hours;
     } else {
-        displayHours = hours;
+        displayedHours = hours;
     }
 
     /* Targeting display to display stopWatch function */
 
-    document.getElementById("display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
+    document.getElementById("display").innerHTML = displayedHours + ":" + displayedMin + ":" + displayedSec;
 }
 
-/* create a function to call when the start button is clicked */
-/* Create a function to call when the stop button is clicked  */
-/* Create a function to call when the reset button is clicked */
+/* Conditional that determines which function are actived by which buttons */
+// if (startBtn) {
+//     watchOnOff = 1;
+// } else if (stopBtn) {
+//     watchOnOff = 2;
+// } else if (resetBtn) {
+//     watchOff = 3;
+// } else {
+//     watchOnOff = watchOnOff;
+// }
 
-// window.setInterval(stopWatch, 1000);
+/* Creating a function for when the start button is clicked */
+function startBtn() {
+        timer = setInterval(stopWatch, 1000);
+}
+
+/* Creating a function for the stop button */
+function stopBtn() {
+        clearInterval(timer);  
+}
+
+/* Creating a reset button */
+/* Setting the original time values to 0 to reset the entire stopWatch function, clearInterval so the stopWatch doesn't reset then keep counting time,
+    and document display = "00:00:00" */
+
+function resetBtn() {
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    clearInterval(timer);
+    document.getElementById("display").innerHTML = "00:00:00";
+}
